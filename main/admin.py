ca103e3a72565838076fcf502ea8b923bb755e55
D'addato <daddatonicolo@itis-molinari.eu>
2026-03-09 14:38:12 +0100

    correzione pagina admin

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Utente, Categoria, Sfida, Flag, Indizio, Partecipa
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+
+class UtenteAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        ('Informazioni CTF', {
+            'fields': ('foto_profilo', 'punteggio', 'livello', 'flag')
+        }),
+    )
+
+admin.site.register(Utente, UtenteAdmin)
+
+models = [Categoria, Sfida, Flag, Indizio, Partecipa]
+for model in models:
+    admin.site.register(model)
+
+
