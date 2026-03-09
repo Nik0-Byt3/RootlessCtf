2d8157f27b0ad61510ec6c3f13f1b7cca9324118
D'addato <daddatonicolo@itis-molinari.eu>
2026-03-09 14:53:18 +0100

    Autenticazione : creazione di login , registrazione e template base per dashboard

---
+++ main/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from .models import Utente
+
+class RegistrazioneForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+    first_name = forms.CharField(required=True, label='Nome')
+    last_name = forms.CharField(required=True, label='Cognome')
+
+    class Meta:
+        model = Utente
+        fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
+
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(label='Username o Email')
--- main/views.py
+++ main/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from .forms import RegistrazioneForm, LoginForm
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+def home(request):
+    return render(request, 'main/home.html')
+
+def registrazione(request):
+    if request.method == 'POST':
+        form = RegistrazioneForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            return redirect('login')
+    else:
+        form = RegistrazioneForm()
+    return render(request, 'main/registrazione.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = LoginForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('dashboard')
+        else:
+            return render(request, 'main/login.html', {'form': form, 'errore': 'Credenziali errate: username o password invalidi'})
+    else:
+        form = LoginForm()
+    return render(request, 'main/login.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
+@login_required
+def dashboard(request):
+    return render(request, 'main/dashboard.html', {'utente': request.user})
